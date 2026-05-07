a13cea621b2cdf6542d8f4eacb0f686605ea1832
marcelchudyba <marcelchudyba7@gmail.com>
2026-05-07 13:07:38 +0200

    added first problem and made structure of a repo

---
+++ 03_stack/0020_valid_parentheses.cpp
@@ -0,0 +1,39 @@
+// Link: https://leetcode.com/problems/valid-parentheses/
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        std::stack<char> opening;
+        std::map<char, char> parentheses;
+
+        parentheses.insert({')', '('});
+        parentheses.insert({']', '['});
+        parentheses.insert({'}', '{'});
+        
+        for(int i = 0; i < s.length(); i++){
+            char curr = s[i];
+
+            if(curr == '(' || curr == '[' || curr == '{'){
+                opening.push(curr);
+            }
+
+            if(curr == ')' || curr == ']' || curr == '}'){
+                if(opening.empty()) return false;
+                char top = opening.top();
+                opening.pop();
+                if(top != parentheses.at(curr)){
+                    return false;
+                }
+                
+            }
+        }
+
+        if(!opening.empty()) return false;
+
+        return true;
+    }
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# Algorytmy i Struktury Danych - LeetCode
+
+Tabela z moim progresem w rozwiązywaniu zadań.
+
+| Numer | Problem | Trudność | Temat | Rozwiązanie |
+|---|---|---|---|---|
+| 0020 | Valid Parentheses | 🟢 Easy | Stack | [C++](./03_stack/0020_valid_parentheses.cpp) |
