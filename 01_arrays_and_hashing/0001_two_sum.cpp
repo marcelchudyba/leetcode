29832421bb20333276ae94c92a1aebb62a53187b
marcelchudyba <marcelchudyba7@gmail.com>
2026-05-08 11:30:22 +0200

    two sum problem

---
+++ 01_arrays_and_hashing/0001_two_sum.cpp
@@ -0,0 +1,28 @@
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //value / index
+        std::unordered_map<int,int> map;
+        int target_to_sum = 0;
+
+        for(int i = 0; i < nums.size();i++){
+            
+            target_to_sum = target - nums[i];
+
+            auto first_index = map.find(target_to_sum);
+
+            if(first_index != map.end() && first_index->second != i){
+                return {first_index->second,i};
+            }
+            map.insert({nums[i], i});
+        }       
+
+        return {};
+        
+    }
+};
